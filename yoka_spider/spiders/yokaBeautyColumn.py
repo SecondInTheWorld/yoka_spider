c3e3f20bb97393957219ec85a82a728b7c4857b6
Administrator <398777640@qq.com>
2019-08-26 13:49:54 +0800

    美容栏目按板块数据爬取

--- yoka_spider/spiders/yokaBeautyColumn.py
+++ yoka_spider/spiders/yokaBeautyColumn.py
@@ -5,7 +5,7 @@ import scrapy
 class YokabeautycolumnSpider(scrapy.Spider):
     name = 'yokaBeautyColumn'
     allowed_domains = ['yoka.com']
-    start_urls = ['http://yoka.com/']
+    # start_urls = ['http://yoka.com/']
 
     def parse(self, response):
         pass
